42ea8c75c913ef359fbf3f3a11306860f771b17d
bazetrip <clintontriplett@westernwyoming.edu>
2024-09-10 02:38:05 -0600

    sales tax

---
+++ projected total sales tax.py
@@ -0,0 +1,10 @@
+# Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the sales tax as 6 percent of total sales.
+sales_tax = total_sales * 0.06
+
+# Display the sales tax.
+print('the sales tax is $', format(sales_tax, ',.2f'))
+
+
